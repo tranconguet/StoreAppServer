7d8c859dffbefe6fdd474f2e42ae7d8603d332ec
tvcong <trancong.uet@gmail.com>
2022-06-26 10:40:58 +0700

    Change password validation param name

--- validation.js
+++ validation.js
@@ -4,7 +4,7 @@ const Joi = require('@hapi/joi');
 const registerValidation = (data) => {
     const schema = Joi.object({
         email: Joi.string().min(6).required(true).email(),
-        password: Joi.string().min(6).required(true),
+        passWord: Joi.string().min(6).required(true),
     })
     return schema.validate(data);
 }
